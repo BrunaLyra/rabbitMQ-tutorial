7e3a25257bbb737b5e93713345ee2a9c3ea33365
BrunaLyra <brunalyra94@gmail.com>
2019-01-22 17:02:55 -0200

    Update send.py

--- lesson-01/send.py
+++ lesson-01/send.py
@@ -1,4 +1,17 @@
 import pika
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
+# Establishing connection with RabbitMQ server
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.chanel() 
+
+# Checking if queue exists
+channel.queue_declare(queue='hello')
+
+# Send message and close connection
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+
+connection.close()
