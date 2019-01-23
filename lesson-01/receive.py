f677aa6cd53db1b4ba2a2730330ffc2efea82303
Luiz Carlos Cosmi FIlho <luizcarloscosmifilho@gmail.com>
2019-01-23 11:34:31 -0200

    --all

--- lesson-01/receive.py
+++ lesson-01/receive.py
@@ -7,10 +7,11 @@ channel = connection.channel()
 # Checking if queue exists
 channel.queue_declare(queue='hello')
 
-# Send message and close connection
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
-print(" [x] Sent 'Hello World!'")
 
-connection.close()
+def callback(ch, method, properties, body):
+    print ("[x] Received %r" %body)
+
+channel.basic_consume(callback, queue='hello', no_ack=True)
+
+print ('[*] Waiting for meassages. To exit press CTRL+C')
+channel.start_consuming()
